11cd2c07e6ae2646f0d6422f6998c2dec906ea77
Arman Bijoy <armanbijoy4@gmail.com>
2021-05-15 16:07:55 +0600

    Changes Done

--- new_todo.php
+++ new_todo.php
@@ -1,7 +1,9 @@
 <?php
-var_dump($_POST);
+// var_dump($_POST);
 
-$todoName = $_POST['todo_name'] ?? false;
+$todoName = $_POST['todo_name'] ?? '';
+
+$todoName = trim($todoName);
 
 if($todoName)
 {
